a13b723d7575207f3a2f1b0f18551ac8ed5ee042
Emre Bayram <emre.gss@gmail.com>
2016-12-04 22:29:13 +0300

    Serial Com Fix

--- SerialCom/main.cpp
+++ SerialCom/main.cpp
@@ -8,8 +8,7 @@ using namespace std;
 int main() {
     Communucation com("/dev/ttyACM0",SerialPort::BR_9600);
     int i;
-    while (1) {
-        com.read();
+    while (com.read()) {
         cout << "motor1 angle " << com.getMotor1Angle() << endl;
         cout << "motor2 angle " << com.getMotor2Angle() << endl;
         cout << " Entry :: " << i++ << endl <<endl;
