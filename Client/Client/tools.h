096f1243841210e8c5d7b7852be679ae42acdfec
Emre Bayram <emre.gss@gmail.com>
2016-12-15 21:04:54 +0300

    Tools

---
+++ Client/Client/tools.h
@@ -0,0 +1,41 @@
+#ifndef TOOL_H
+#define TOOL_H
+#include"Communucation.h"
+/*
+            Thread (grafik çizme)
+              ^
+        pipe  |
+              |
+              |    Serial
+    Host-> Thread -----> Arduiono
+              |
+              |pipe
+              |
+              |     fifo
+            Thread -----> Unreal Exe
+                            */
+namespace Tool {
+
+    //TODO:
+    /*Maybe Missing something*/
+    struct threadMessageArdu{
+        char* portname;
+    };
+
+    //TODO:
+    /*Maybe Missing something*/
+    struct threadMessage3D{
+        int pipeid;
+    };
+
+    //TODO:
+    /*Maybe Missing something*/
+    struct threadMessageGrafik {
+        int pipeid;
+    };
+
+    void CommunucateWithArduino(void* message);
+    void CommunucateWith3DSim(void* message);
+    void CommunucateWithGrafik(void* message);
+}
+#endif // TOOL_H
